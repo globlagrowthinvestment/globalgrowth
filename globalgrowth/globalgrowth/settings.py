be09959e9ebce80858dcb3d49dadc7420129528b
Ndegwadavid <davidndegwa6869@gmail.com>
2024-07-07 02:04:25 +0300

    changes

--- globalgrowth/globalgrowth/settings.py
+++ globalgrowth/globalgrowth/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'globalgrowth.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:heffen@localhost:5432/globalgrowth',
+        default='postgresql://postgres:kali@localhost:5432/globalgrowth',
         conn_max_age=600
     )
 }
@@ -172,7 +172,7 @@ STATIC_URL = '/static/'
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'globalgrowth/static')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
