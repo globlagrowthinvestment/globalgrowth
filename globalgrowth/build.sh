242c2a074e65f6427fcbe7bfcaeb2e12976494a8
Peetah254 <peternyando2@gmail.com>
2024-08-04 21:24:18 +0300

    aaaa

--- globalgrowth/build.sh
+++ globalgrowth/build.sh
@@ -2,20 +2,25 @@
 # Exit on error
 set -o errexit
 
+# Set up virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
 # Upgrade pip
 pip install --upgrade pip
 
+# Install AWS CLI (if needed)
 pip install awscli
 
-pip install --upgrade setuptools pip
-pip install --upgrade wheel
+# Upgrade setuptools and wheel
+pip install --upgrade setuptools wheel
 
 # Install requirements
 pip install -r requirements.txt
 
-
-# Collect static files (if applicable)
+# Collect static files (if applicable for Django projects)
 python manage.py collectstatic --no-input
 
-# Apply database migrations (if applicable)
+# Apply database migrations (if applicable for Django projects)
 python manage.py migrate
+
